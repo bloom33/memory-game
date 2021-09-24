9bd6cc9726222bc25d214d812497c70cf94c479a
bloom <bloomart11@gmail.com>
2021-09-24 19:49:32 -0400

    Created an array of card names.

---
+++ app.js
@@ -0,0 +1,59 @@
+//dom event listener: card options //
+
+//array of card-names//
+//make two of each, because it's a matching game.//
+const cardArray = [
+  {
+    name: 'fries',
+    img: 'images/fries.png'
+  },
+  {
+    name: 'fries',
+    img: 'images/fries.png'
+  },
+
+  {
+    name: 'cheeseburger',
+    img: 'images/cheeseburger.png'
+  },
+  {
+    name: 'cheeseburger',
+    img: 'images/cheeseburger.png'
+  },
+
+  {
+    name: 'hotdog',
+    img: 'images/hotdog.png'
+  },
+  {
+    name: 'hotdog',
+    img: 'images/hotdog.png'
+  },
+
+  {
+    name: 'ice-cream',
+    img: 'images/ice-cream.png'
+  },
+  {
+    name: 'ice-cream',
+    img: 'images/ice-cream.png'
+  },
+
+  {
+    name: 'milkshake',
+    img: 'images/milkshake.png'
+  },
+  {
+    name: 'milkshake',
+    img: 'images/milkshake.png'
+  },
+
+  {
+    name: 'pizza',
+    img: 'images/pizza.png'
+  },
+  {
+    name: 'pizza',
+    img: 'images/pizza.png'
+  },
+]
